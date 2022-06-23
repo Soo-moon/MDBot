64d1b2d7a1fa90ce43d251d54d88ed463a859bb6
me <eck5689@naver.com>
2022-06-23 16:05:35 +0900

    else error 해결

--- src/main/kotlin/MessageListener.kt
+++ src/main/kotlin/MessageListener.kt
@@ -4,35 +4,37 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 
 class MessageListener : ListenerAdapter() {
-    private val tag : String ="myLog//MessageListener"
+    private val tag: String = "myLog//MessageListener"
     private val log = Mlog()
     override fun onMessageReceived(event: MessageReceivedEvent) {
         val msg = event.message
         val channel = event.channel
         val user = event.author
 
-        if(msg.contentRaw[0] == '!'){
+        if (msg.contentRaw[0] == '!') {
             val args = msg.contentRaw.substring(1).split(" ")
             //log
-            print("$tag || ${log.getTime()} || username : ${user.name} message : $msg \n")
-            if (args[0] == "경매" || args[0] == "ㄱㅁ"){
-                val data = args[1].toInt()
-                val eb = EmbedBuilder()
+            print("$tag || ${log.getTime()} || username : ${user.name} message : ${msg.contentRaw} \n")
+            if (user.name != "MDBot") {
+                if (args[0] == "경매" || args[0] == "ㄱㅁ") {
+                    val data = args[1].toInt()
+                    val eb = EmbedBuilder()
 
-                eb.setTitle("경매 계산기")
-                eb.addField("4인N빵" , "${(data * 0.7215).toInt()}gold" , true)
-                eb.addField("4인선점" , "${(data * 0.64772).toInt()}gold" , true)
-                eb.addBlankField(false)
-                eb.addField("8인N빵" , "${(data * 0.83125).toInt()}gold" , true)
-                eb.addField("8인선점" , "${(data * 0.75568).toInt()}gold" , true)
+                    eb.setTitle("경매 계산기")
+                    eb.addField("4인N빵", "${(data * 0.7215).toInt()}gold", true)
+                    eb.addField("4인선점", "${(data * 0.64772).toInt()}gold", true)
+                    eb.addBlankField(false)
+                    eb.addField("8인N빵", "${(data * 0.83125).toInt()}gold", true)
+                    eb.addField("8인선점", "${(data * 0.75568).toInt()}gold", true)
 
-                channel.sendMessageEmbeds(eb.build()).queue()
+                    channel.sendMessageEmbeds(eb.build()).queue()
+                } else if (args[0] == "?" || args[0] == "도움" || args[0] == "help") {
+                    channel.sendMessage("명령어 리스트 ").queue()
+                    channel.sendMessage("!경매, !ㄱㅁ  ==> 경매분배금 계산 기능(4인,8인)").queue()
+                } else {
+                    channel.sendMessage("명령어리스트는 !?, !도움, !help 을 입력하세요.").queue()
+                }
             }
-            else if(args[0] == "?" || args[0] == "도움" || args[0] == "help"){
-                channel.sendMessage("명령어 리스트 ").queue()
-                channel.sendMessage("!경매, !ㄱㅁ  ==> 경매분배금 계산 기능(4인,8인)").queue()
-            }
-            else channel.sendMessage("명령어리스트는 !?, !도움, !help 을 입력하세요.").queue()
         }
     }
 }
