e9fcb1d742eccf3e7491eab3a8c6302ab400b3a1
me <eck5689@naver.com>
2022-07-21 17:47:33 +0900

    .

--- src/main/kotlin/Mlog.kt
+++ src/main/kotlin/Mlog.kt
@@ -3,7 +3,7 @@ import java.time.format.DateTimeFormatter
 
 class Mlog {
 
-    fun getTime(): String {
+    fun time(): String {
         val current = LocalDateTime.now()
         val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")
         return current.format(formatter)
